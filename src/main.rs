a5fa8da0c26ba1c521cba867d3757521cdbef43c
Ofri Haviv <haviv.ofri@gmail.com>
2023-01-12 01:10:52 +0200

    Changed main

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use crate::matrix::ToMatrix;
 pub mod matrix;
 fn main() {
     let x= [[1,0,0],[0,1,0],[0,0,1]].to_matrix();
-    
+    let y = [[1,2,3],[4,5,6],[7,8,9]].to_matrix();
+    let y = &x * &x * &y * 8.0;
+
+    println!("{}",y);
     // let y = &x*&x;
     println!("hello world");
 }

11228b3e45ed4dd10b18af24f8a98b4cc347cd90
Ofri Haviv <haviv.ofri@gmail.com>
2024-11-12 14:44:30 +0200

    nice

--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,14 @@ fn main() {
         epochs: 100,
         single_epochs: 10,
         op_args: GradientDecentOptimizerArgs {
-            learning_rate: 0.1, 
+            learning_rate: 0.01, 
         },
     };
     let mut net = network! {
             optimizer: GradientDecentOptimizer,
             layers: [
-                 (2,3,TanhActivation),
-                 (3,1,TanhActivation)
+                 (2,10,TanhActivation),
+                 (10,1,LeakyReLUActivation)
             ]
     };
     let mut x = Matrix::<2, 4>::default();
@@ -43,13 +43,13 @@ fn main() {
             x.set_sub(j + i * 2, &Matrix::from([[i as f64, j as f64]]));
             y.set_sub(
                 j + i * 2,
-                &Matrix::from([[if i == j { 1 } else { -1 }]]),
+                &Matrix::from([[if i != j { 1 } else { -1 }]]),
             )
         }
     }
 
     net.fit(x, y, &args);
-    println!("{:?}", net);
+    // println!("{:?}", net);
     println!("{}", net.predict([[0, 1]].into()));
     println!("{}", net.predict([[1, 0]].into()));
     println!("{}", net.predict([[1, 1]].into()));
